3211e2af6b460e9f28be72df6c6dc700e37fa19b
Alycia <alycin2@uw.edu>
2021-04-18 15:44:20 -0700

    Read Data

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import React from "react";
+import { csv } from "d3-fetch";
 
 const viewHeight = 500;
 const viewWidth = 500;
 
 
 const App = () => {
+    // Adding data to application 
+    csv('https://raw.githubusercontent.com/alycianguyenn/a2-exploratory-data-analysis/main/avocado.csv')
+        .then(data => console.log(data));
     return (
-        <svg 
-            style={{border: "1px solid lightgrey", width: viewWidth,
-            height: viewHeight}}
-        >
-            <circle cx={20} cy={20} r="5" />
-            <rect x={200} y={200} width={10} height={10} fill="rgb(230, 230, 230)"/>
-            <rect x={212} y={200} width={10} height={10} fill="rgb(230, 230, 230)"/>
-            <rect x={224} y={200} width={10} height={10} fill="rgb(230, 230, 230)"/>
-            <rect x={236} y={200} width={10} height={10} fill="rgb(230, 230, 230)"/>
-            <rect x={248} y={200} width={10} height={10} />
-            <line x1={20} y1={viewHeight - 20} x2={150} y2={100} stroke="black" />
-            <text x="20" y="35">
-                Price history of 100 randomly selected Pokemon cards
-            </text>
-        </svg>
-    ); // outer brace = js / inner brace = object
+        <div>
+            <h1>Avocado Data Exploratory Analysis</h1>
+            <p>Data!</p>
+        </div>
+    ); 
 };
 
 export default App;
