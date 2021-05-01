8a2fab4b5434f23b05a0701b4dd34f5fd798ddbf
Alycia <alycin2@uw.edu>
2021-05-01 00:18:31 -0700

    Add initial

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { csv } from "d3-fetch";
+// import { csv } from "d3-fetch";
 
 const viewHeight = 500;
 const viewWidth = 500;
@@ -7,8 +7,8 @@ const viewWidth = 500;
 
 const App = () => {
     // Adding data to application 
-    csv('https://raw.githubusercontent.com/alycianguyenn/a2-exploratory-data-analysis/main/avocado.csv')
-        .then(data => console.log(data));
+    // csv('https://raw.githubusercontent.com/alycianguyenn/a2-exploratory-data-analysis/main/avocado.csv')
+    //     .then(data => console.log(data));
     return (
         <div>
             <h1>Avocado Data Exploratory Analysis</h1>
